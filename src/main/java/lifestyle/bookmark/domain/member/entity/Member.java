554ceea87a9577832a084515e141a4cabd031142
KimHuiMang <piyrw9754@gmail.com>
2022-12-30 11:28:33 +0900

    :memo: Member 엔티티 추가
    멤버 엔티티 추가

---
+++ src/main/java/lifestyle/bookmark/domain/member/entity/Member.java
@@ -0,0 +1,23 @@
+package lifestyle.bookmark.domain.member.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    private String loginId;
+    private String password;
+    private String name;
+    private String email;
+}
