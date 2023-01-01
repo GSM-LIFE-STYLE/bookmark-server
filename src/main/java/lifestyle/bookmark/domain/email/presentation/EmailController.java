4865dceb926aa3d826461091029bae4324cfb0d8
KimHuiMang <piyrw9754@gmail.com>
2023-01-01 14:14:10 +0900

    :construction: 이메일 체크 컨트롤러 등록
    컨트롤러 등록

--- src/main/java/lifestyle/bookmark/domain/email/presentation/EmailController.java
+++ src/main/java/lifestyle/bookmark/domain/email/presentation/EmailController.java
@@ -5,12 +5,10 @@ import lifestyle.bookmark.domain.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +21,11 @@ public class EmailController {
     public ResponseEntity<Void> sendEmail(@Valid @RequestBody SendEmailRequest request) {
         emailService.sendEmail(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @RequestMapping(method = RequestMethod.HEAD)
+    public ResponseEntity<Void> checkEmail(@Email @RequestParam String email, @RequestParam String authKey) {
+        emailService.checkEmail(email, authKey);
+        return ResponseEntity.ok().build();
     }
 }
