4c55a29a4cc6e7e12ef4328b49f6115fba87f6aa
KimHuiMang <piyrw9754@gmail.com>
2023-01-02 16:36:36 +0900

    :construction: 리프레시 토큰 발급
    리프레시 토큰 발급

--- src/main/java/lifestyle/bookmark/domain/auth/presentation/dto/response/TokenResponse.java
+++ src/main/java/lifestyle/bookmark/domain/auth/presentation/dto/response/TokenResponse.java
@@ -16,5 +16,5 @@ public class TokenResponse {
     private final String refreshToken;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private final ZonedDateTime expireAt;
+    private final ZonedDateTime expiredAt;
 }
