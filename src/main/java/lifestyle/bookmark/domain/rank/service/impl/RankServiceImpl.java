fa5fe103b365ff921e50800d1aee1c48e738b0c0
KimHuiMang <piyrw9754@gmail.com>
2023-01-08 15:28:47 +0900

    :sparkles: 랭크 카테고리별 엔티티 생성 서비스
    랭크 카테고리별 엔티티 생성 서비스

--- src/main/java/lifestyle/bookmark/domain/rank/service/impl/RankServiceImpl.java
+++ src/main/java/lifestyle/bookmark/domain/rank/service/impl/RankServiceImpl.java
@@ -1,10 +1,20 @@
 package lifestyle.bookmark.domain.rank.service.impl;
 
+import lifestyle.bookmark.domain.member.domain.Member;
+import lifestyle.bookmark.domain.member.domain.RankMember;
 import lifestyle.bookmark.domain.member.domain.repository.MemberRepository;
+import lifestyle.bookmark.domain.member.domain.repository.RankMemberRepository;
+import lifestyle.bookmark.domain.member.exception.MemberNotFoundException;
+import lifestyle.bookmark.domain.rank.domain.Rank;
 import lifestyle.bookmark.domain.rank.domain.repository.RankRepository;
+import lifestyle.bookmark.domain.rank.exception.NotFoundRankException;
 import lifestyle.bookmark.domain.rank.service.RankService;
+import lifestyle.bookmark.global.enum_type.rank_category.RankCategory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,14 +22,44 @@ public class RankServiceImpl implements RankService {
 
     private final MemberRepository memberRepository;
     private final RankRepository rankRepository;
+    private final RankMemberRepository rankMemberRepository;
+
+    private List<Member> getSortMembers() {
+        List<Member> members = memberRepository.findAll();
+        if(members.isEmpty())
+            throw new MemberNotFoundException("멤버가 존재하지 않습니다.");
+
+        return members;
+    }
+
+    private void createRankMember(List<Member> members, Rank rank) {
+        for (Member member : members) {
+            RankMember rankMember = RankMember.builder().member(member).rank(rank).build();
+            rankMemberRepository.save(rankMember);
+        }
+    }
 
     @Override
     public void createPageRank() {
+        List<Member> members = getSortMembers();
 
+        Rank rank = Rank.builder()
+                .members(members)
+                .rankCategory(RankCategory.PAGE)
+                .build();
+
+        createRankMember(members , rank);
     }
 
     @Override
     public void createBookRank() {
+        List<Member> members = getSortMembers();
 
+        Rank rank = Rank.builder()
+                .members(members)
+                .rankCategory(RankCategory.BOOK)
+                .build();
+
+        createRankMember(members , rank);
     }
 }
